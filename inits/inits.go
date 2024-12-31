4405beff4aa848b8b77a98ab6a4a8701509b2aad
deniskorbakov <korbakovd@gmail.com>
2024-12-31 18:12:35 +0300

    #141 update order import pkg

--- inits/inits.go
+++ inits/inits.go
@@ -2,9 +2,9 @@ package inits
 
 import (
 	"github.com/spf13/viper"
-	"github.com/ssh-connection-manager/kernel/v2/internal/logger"
 
 	"github.com/ssh-connection-manager/kernel/v2/config"
+	"github.com/ssh-connection-manager/kernel/v2/internal/logger"
 	"github.com/ssh-connection-manager/kernel/v2/pkg/crypt"
 	"github.com/ssh-connection-manager/kernel/v2/pkg/file"
 	"github.com/ssh-connection-manager/kernel/v2/pkg/json"
