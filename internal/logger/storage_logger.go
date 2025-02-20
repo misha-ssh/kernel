8c3ad722d651314e0e5c907a2c78137f0b2a72c5
Denis <korbakovd@gmail.com>
2025-02-20 21:49:25 +0300

    #165 create storage logger

--- internal/logger/storage_logger.go
+++ internal/logger/storage_logger.go
@@ -1,7 +1,52 @@
 package logger
 
-type StorageLogger struct{}
+import (
+	"errors"
+	"fmt"
+	"github.com/ssh-connection-manager/kernel/v2/pkg/storage"
+	"log"
+	"runtime"
+)
 
-func (l *StorageLogger) Log(key string, value interface{}) {
+const (
+	SkipUseLevel = 1
+	NameLogFile  = "log.log"
+)
 
+var (
+	ErrCreateStorage = errors.New("err at created log file")
+	ErrGetOpenFile   = errors.New("err get open log file")
+	ErrGetInfo       = errors.New("err get info use log")
+)
+
+type StorageLogger struct {
+	Storage storage.Storage
+}
+
+func (sl *StorageLogger) log(value any) error {
+	err := sl.Storage.Create(NameLogFile)
+	if err != nil {
+		return ErrCreateStorage
+	}
+
+	_, calledFile, line, success := runtime.Caller(SkipUseLevel)
+	if !success {
+		return ErrGetInfo
+	}
+
+	logInfo := fmt.Sprintf("file: %s, line: %v, message: %#v", calledFile, line, value)
+
+	openFile, err := sl.Storage.GetOpenFile(NameLogFile)
+	if err != nil {
+		return ErrGetOpenFile
+	}
+
+	errorLog := log.New(openFile, "", log.LstdFlags|log.Lmicroseconds)
+	errorLog.Println(logInfo)
+
+	return nil
+}
+
+func (sl *StorageLogger) Error(value any) {
+	_ = sl.log(value)
 }
