19b1555ec220216e2b4fce7f73fa5b67e3357317
Denis <korbakovd@gmail.com>
2025-02-22 22:19:08 +0300

    #165 update methods

--- internal/encryption/storage_encryption.go
+++ internal/encryption/storage_encryption.go
@@ -10,40 +10,48 @@ import (
 const SizeKey = 64
 
 var (
-	ErrGenerateKey = errors.New("err at created log file")
+	ErrGenerateKey    = errors.New("err at created log file")
+	ErrVerifyKEy      = errors.New("err verify key on standard aes")
+	ErrBlockCipher    = errors.New("err create 128-bit block cipher")
+	ErrRandRead       = errors.New("err rand read encrypt")
+	ErrAuthCiphertext = errors.New("err open decrypts and authenticates ciphertext")
 )
 
 type StorageEncryption struct{}
 
-func (s *StorageEncryption) Encrypt(plaintext []byte, key []byte) ([]byte, error) {
+func (s *StorageEncryption) getGcm(key []byte) (cipher.AEAD, error) {
 	newCipher, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err)
+		return nil, ErrVerifyKEy
 	}
 
 	gcm, err := cipher.NewGCM(newCipher)
 	if err != nil {
-		panic(err)
+		return nil, ErrBlockCipher
+	}
+
+	return gcm, nil
+}
+
+func (s *StorageEncryption) Encrypt(plaintext []byte, key []byte) ([]byte, error) {
+	gcm, err := s.getGcm(key)
+	if err != nil {
+		return nil, err
 	}
 
 	nonce := make([]byte, gcm.NonceSize())
 	_, err = rand.Read(nonce)
 	if err != nil {
-		panic(err)
+		return nil, ErrRandRead
 	}
 
 	return gcm.Seal(nonce, nonce, plaintext, nil), nil
 }
 
 func (s *StorageEncryption) Decrypt(ciphertext []byte, key []byte) ([]byte, error) {
-	newCipher, err := aes.NewCipher(key)
+	gcm, err := s.getGcm(key)
 	if err != nil {
-		panic(err)
-	}
-
-	gcm, err := cipher.NewGCM(newCipher)
-	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	nonceSize := gcm.NonceSize()
@@ -51,7 +59,7 @@ func (s *StorageEncryption) Decrypt(ciphertext []byte, key []byte) ([]byte, erro
 
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
-		panic(err)
+		return nil, ErrAuthCiphertext
 	}
 
 	return plaintext, nil
@@ -66,4 +74,8 @@ func (s *StorageEncryption) GenerateKey() ([]byte, error) {
 	}
 
 	return key, nil
+}
+
+func (s *StorageEncryption) GetKey() ([]byte, error) {
+	return nil, nil
 }
