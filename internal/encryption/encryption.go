2d261d921e244af94f3935db927531888809ec23
Denis <korbakovd@gmail.com>
2025-02-22 22:17:34 +0300

    #165 update name package and add interface

--- internal/encryption/encryption.go
+++ internal/encryption/encryption.go
@@ -1,7 +1,8 @@
 package encryption
 
-type encryption interface {
+type Encryption interface {
 	Encrypt(plaintext []byte, key []byte) ([]byte, error)
 	Decrypt(ciphertext []byte, key []byte) ([]byte, error)
 	GenerateKey() ([]byte, error)
+	GetKey() ([]byte, error)
 }
