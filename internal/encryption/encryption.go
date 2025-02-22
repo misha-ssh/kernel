43e4a0e5025f73b00aec69dba42d73c27c1eb71a
Denis <korbakovd@gmail.com>
2025-02-22 17:13:35 +0300

    #165 update name method

--- internal/encryption/encryption.go
+++ internal/encryption/encryption.go
@@ -3,5 +3,5 @@ package encryption
 type encryption interface {
 	Encrypt(plaintext []byte, key []byte) ([]byte, error)
 	Decrypt(ciphertext []byte, key []byte) ([]byte, error)
-	GetKey() ([]byte, error)
+	GenerateKey() ([]byte, error)
 }
