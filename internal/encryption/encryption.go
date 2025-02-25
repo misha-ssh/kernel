d42be26b3f06470f9bed4214bb547f6e506703c5
Denis <korbakovd@gmail.com>
2025-02-25 23:57:47 +0300

    #165 create mock

--- internal/encryption/encryption.go
+++ internal/encryption/encryption.go
@@ -1,8 +1,54 @@
 package encryption
 
+import (
+	"github.com/ssh-connection-manager/kernel/v2/pkg/storage"
+	"github.com/stretchr/testify/mock"
+)
+
 type Encryption interface {
-	Encrypt(plaintext []byte, key []byte) ([]byte, error)
-	Decrypt(ciphertext []byte, key []byte) ([]byte, error)
-	GenerateKey() ([]byte, error)
-	GetKey() ([]byte, error)
+	Encrypt(plaintext string, key string) (string, error)
+	Decrypt(ciphertext string, key string) (string, error)
+	GenerateKey() (string, error)
+	GetKey(storage storage.Storage) (string, error)
+}
+
+type MockEncryption struct {
+	mock.Mock
+}
+
+func (m *MockEncryption) Encrypt(plaintext string, key string) (string, error) {
+	args := m.Called(plaintext, key)
+
+	if args.Error(1) != nil {
+		return "", args.Error(1)
+	}
+
+	return args.Get(0).(string), args.Error(1)
+}
+
+func (m *MockEncryption) Decrypt(ciphertext string, key string) (string, error) {
+	args := m.Called(ciphertext, key)
+	if args.Error(1) != nil {
+		return "", args.Error(1)
+	}
+
+	return args.Get(0).(string), args.Error(1)
+}
+
+func (m *MockEncryption) GenerateKey() (string, error) {
+	args := m.Called()
+	if args.Error(1) != nil {
+		return "", args.Error(1)
+	}
+
+	return args.Get(0).(string), args.Error(1)
+}
+
+func (m *MockEncryption) GetKey(storage storage.Storage) (string, error) {
+	args := m.Called(storage)
+	if args.Error(1) != nil {
+		return "", args.Error(1)
+	}
+
+	return args.Get(0).(string), args.Error(1)
 }
