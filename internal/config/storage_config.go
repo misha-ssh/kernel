8eea1e68c6c3eb4263734268c558eac5ac5321df
deniskorbakov <korbakovd@gmail.com>
2025-04-16 00:43:23 +0300

    #165 update logic

--- internal/config/storage_config.go
+++ internal/config/storage_config.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"os"
+	"errors"
 	"strings"
 
 	"github.com/ssh-connection-manager/kernel/v2/internal/logger"
@@ -16,40 +16,60 @@ const (
 
 var s *StorageConfig
 
+var (
+	ErrWriteDataToOpenFile = errors.New("write data to open file error")
+	ErrCreateConfigFile    = errors.New("create config file error")
+	ErrGetOpenFile         = errors.New("get open file error")
+)
+
 type StorageConfig struct {
 	Storage storage.Storage
 }
 
-func (s *StorageConfig) createConfig() {
+func (s *StorageConfig) createConfig() error {
 	if !s.Storage.Exists(FileName) {
 		err := s.Storage.Create(FileName)
 		if err != nil {
-			logger.Error(err.Error())
+			logger.Error(ErrCreateConfigFile)
+			return ErrCreateConfigFile
 		}
 	}
+
+	return nil
 }
 
-func (s *StorageConfig) validateData()
+func (s *StorageConfig) validateData(key, value string) error {
+	return nil
+}
 
-func Set(key, value string) { s.Set(key, value) }
+func Set(key, value string) error { return s.Set(key, value) }
 
-func (s *StorageConfig) Set(key, value string) {
-	s.createConfig()
+func (s *StorageConfig) Set(key, value string) error {
+	err := s.validateData(key, value)
+	if err != nil {
+		return err
+	}
+
+	err = s.createConfig()
+	if err != nil {
+		return err
+	}
 
 	param := strings.ToUpper(key) + Separator + value + "\n"
 
 	openConfigFile, err := s.Storage.GetOpenFile(FileName)
-	defer func(openConfigFile *os.File) {
-		err = openConfigFile.Close()
-	}(openConfigFile)
 	if err != nil {
-		logger.Error(err.Error())
+		logger.Error(ErrGetOpenFile)
+		return ErrGetOpenFile
 	}
 
-	_, err = openConfigFile.WriteString(param)
+	err = s.Storage.WriteToOpenFile(openConfigFile, param)
 	if err != nil {
-		logger.Error(err.Error())
+		logger.Error(ErrWriteDataToOpenFile)
+		return ErrWriteDataToOpenFile
 	}
+
+	return nil
 }
 
 func Get(key string) string { return s.Get(key) }
