df2523687790f8c5ed741a99c7dab49f41419c51
Denis <korbakovd@gmail.com>
2025-04-15 17:39:59 +0300

    #165 update logic storage config

--- internal/config/storage_config.go
+++ internal/config/storage_config.go
@@ -1,47 +1,71 @@
 package config
 
 import (
+	"os"
+	"strings"
+
 	"github.com/ssh-connection-manager/kernel/v2/internal/logger"
 	"github.com/ssh-connection-manager/kernel/v2/pkg/storage"
-	"strings"
 )
 
 const (
-	FileName = "config.txt"
+	FileName   = "config.txt"
+	EmptyValue = ""
+	Separator  = "="
 )
 
-var l *StorageConfig
+var s *StorageConfig
 
 type StorageConfig struct {
 	Storage storage.Storage
 }
 
-func Get(key string) string { return l.Get(key) }
-
-func (l *StorageConfig) Get(key string) string {
-	return ""
-}
-
-func Set(key, value string) { l.Set(key, value) }
-
-func (l *StorageConfig) Set(key, value string) {
-	if !l.Storage.Exists(FileName) {
-		err := l.Storage.Create(FileName)
+func (s *StorageConfig) createConfig() {
+	if !s.Storage.Exists(FileName) {
+		err := s.Storage.Create(FileName)
 		if err != nil {
 			logger.Error(err.Error())
 		}
 	}
+}
 
-	param := strings.ToUpper(key) + "=" + value
+func (s *StorageConfig) validateData()
 
-	err := l.Storage.Write(FileName, param)
+func Set(key, value string) { s.Set(key, value) }
+
+func (s *StorageConfig) Set(key, value string) {
+	s.createConfig()
+
+	param := strings.ToUpper(key) + Separator + value + "\n"
+
+	openConfigFile, err := s.Storage.GetOpenFile(FileName)
+	defer func(openConfigFile *os.File) {
+		err = openConfigFile.Close()
+	}(openConfigFile)
+	if err != nil {
+		logger.Error(err.Error())
+	}
+
+	_, err = openConfigFile.WriteString(param)
 	if err != nil {
 		logger.Error(err.Error())
 	}
 }
 
-func Exists(key string) bool { return l.Exists(key) }
+func Get(key string) string { return s.Get(key) }
 
-func (l *StorageConfig) Exists(key string) bool {
+func (s *StorageConfig) Get(key string) string {
+	got, err := s.Storage.Get(FileName)
+	if err != nil {
+		logger.Error(err.Error())
+		return EmptyValue
+	}
+
+	return got
+}
+
+func Exists(key string) bool { return s.Exists(key) }
+
+func (s *StorageConfig) Exists(key string) bool {
 	return true
 }
