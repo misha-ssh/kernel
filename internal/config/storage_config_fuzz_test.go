d203b83f6a1b0455bb0f07201d3b401323042877
deniskorbakov <korbakovd@gmail.com>
2025-04-22 00:44:38 +0300

    #165 create logic for fuzz test

--- internal/config/storage_config_fuzz_test.go
+++ internal/config/storage_config_fuzz_test.go
@@ -1,1 +1,33 @@
 package config
+
+import (
+	"testing"
+
+	"github.com/ssh-connection-manager/kernel/v2/pkg/storage"
+	"github.com/stretchr/testify/assert"
+)
+
+func FuzzLocalStorage_Set(f *testing.F) {
+	localStorage := storage.LocalStorage{
+		Direction: f.TempDir(),
+	}
+
+	f.Fuzz(func(t *testing.T, value string) {
+		key := "TEST"
+
+		s := StorageConfig{
+			Storage: &localStorage,
+		}
+
+		_ = s.Set(key, value)
+
+		got := s.Get(key)
+		validateErr := validateValue(value)
+
+		if validateErr != nil {
+			assert.Equal(t, EmptyValue, got)
+		} else {
+			assert.Equal(t, value, got)
+		}
+	})
+}
