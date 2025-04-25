c98c676cb82d5b73f8aee80a214265e366c61cbc
deniskorbakov <korbakovd@gmail.com>
2025-04-26 00:30:15 +0300

    #165 add constant

--- configs/envconst/constant.go
+++ configs/envconst/constant.go
@@ -1,21 +1,5 @@
 package envconst
 
 const (
-	// tools name.
-	AgnosticName   = "tf"
-	AtmosName      = "atmos"
-	TenvName       = "tenv"
-	TerraformName  = "terraform"
-	TerragruntName = "terragrunt"
-	TofuName       = "tofu"
-
-	// full project name.
-	OpentofuName = "opentofu"
-
-	// hidden proxy sub command.
-	CallSubCmd = "call"
-
-	BasicErrorExitCode = 1
-	// exit code used by tenv proxy to signal a failure before the proxied command call.
-	EarlyErrorExitCode = 42
+	AppName = "ssh+"
 )
