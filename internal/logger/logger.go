2ac77aa06b2625e3bc647c79ccd99bc1a8fb5c7d
deniskorbakov <korbakovd@gmail.com>
2025-04-26 18:32:45 +0300

    #165 update returned type

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,8 +1,21 @@
 package logger
 
+import "errors"
+
 type Logger interface {
-	Error(value any) error
-	Debug(value any) error
-	Info(value any) error
-	Warn(value any) error
+	Error(value any)
+	Debug(value any)
+	Info(value any)
+	Warn(value any)
 }
+
+const (
+	SkipUseLevel = 1
+	FileName     = "log.log"
+)
+
+var (
+	ErrCreateStorage = errors.New("err at created log file")
+	ErrGetOpenFile   = errors.New("err get open log file")
+	ErrGetInfo       = errors.New("err get info use log")
+)
