6536204efe9cf74b261882a78c972967b06b2f25
Denis <korbakovd@gmail.com>
2025-05-17 15:37:30 +0300

    #165 create file for test ssh conenct

---
+++ internal/connect/ssh_test.go
@@ -0,0 +1,23 @@
+package connect
+
+import "testing"
+
+func TestSshConnect_Connect(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "success",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &SshConnect{}
+			if err := s.Connect(); (err != nil) != tt.wantErr {
+				t.Errorf("Connect() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
