e9c95ac54e2fb829b4677572d1f63df79c776b19
Denis <korbakovd@gmail.com>
2025-05-23 01:04:30 +0300

    #165 delete extra method

--- internal/crypto/crypto.go
+++ internal/crypto/crypto.go
@@ -1,12 +1,7 @@
 package crypto
 
-import (
-	"github.com/ssh-connection-manager/kernel/v2/internal/storage"
-)
-
 type Crypto interface {
 	Encrypt(plaintext string, key string) (string, error)
 	Decrypt(ciphertext string, key string) (string, error)
 	GenerateKey() (string, error)
-	GetKey(storage storage.Storage) (string, error)
 }
