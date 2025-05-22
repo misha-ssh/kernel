5226e7b71a2708a6f9794da98ba0f75fa70e4be9
Denis <korbakovd@gmail.com>
2025-05-23 01:05:14 +0300

    #165 create new methods for config

--- config/config.go
+++ config/config.go
@@ -1,10 +1,78 @@
 package config
 
-import "github.com/ssh-connection-manager/kernel/v2/internal/init"
+import (
+	"errors"
+
+	"github.com/ssh-connection-manager/kernel/v2/config/envconst"
+	"github.com/ssh-connection-manager/kernel/v2/config/envname"
+	"github.com/ssh-connection-manager/kernel/v2/internal/config"
+	"github.com/ssh-connection-manager/kernel/v2/internal/logger"
+	"github.com/ssh-connection-manager/kernel/v2/internal/storage"
+)
+
+var ErrGetConsoleInfo = errors.New("err set default value")
+
+func setDefaultValues(config config.Config) error {
+	if !config.Exists(envname.Theme) {
+		err := config.Set(envname.Theme, envconst.Theme)
+		if err != nil {
+			logger.Error(ErrGetConsoleInfo)
+			return ErrGetConsoleInfo
+		}
+	}
+
+	return nil
+}
+
+func initFileConnections() error {
+	fileName := envconst.FilenameConnection
+
+	fileStorage := &storage.FileStorage{
+		Direction: storage.GetHomeDir(),
+	}
+
+	if !fileStorage.Exists(fileName) {
+		err := fileStorage.Create(envconst.FilenameConnection)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func initFileConfig() error {
+	fileStorage := &storage.FileStorage{
+		Direction: storage.GetHomeDir(),
+	}
+
+	fileConfig := &config.StorageConfig{
+		Storage: fileStorage,
+	}
+
+	err := setDefaultValues(fileConfig)
+	if err != nil {
+		logger.Error(err)
+		return err
+	}
+
+	return nil
+}
+
+func generateCryptKey() error {
+	return nil
+}
 
 func Init() error {
-	err := init.FileConfig()
+	err := initFileConfig()
 	if err != nil {
+		logger.Error(err)
+		return err
+	}
+
+	err = initFileConnections()
+	if err != nil {
+		logger.Error(err)
 		return err
 	}
 
