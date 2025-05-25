e531e53d0381b754576cadad90178554cf3b777f
Denis <korbakovd@gmail.com>
2025-05-25 20:04:52 +0300

    #165 update logic file

--- internal/crypto/crypto.go
+++ internal/crypto/crypto.go
@@ -1,7 +1,86 @@
 package crypto
 
-type Crypto interface {
-	Encrypt(plaintext string, key string) (string, error)
-	Decrypt(ciphertext string, key string) (string, error)
-	GenerateKey() (string, error)
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+
+	"github.com/ssh-connection-manager/kernel/v2/internal/logger"
+)
+
+const SizeKey = 32
+
+var (
+	ErrGenerateKey    = errors.New("err at created log file")
+	ErrVerifyKEy      = errors.New("err verify key on standard aes")
+	ErrBlockCipher    = errors.New("err create 128-bit block cipher")
+	ErrRandRead       = errors.New("err rand read encrypt")
+	ErrAuthCiphertext = errors.New("err open decrypts and authenticates ciphertext")
+)
+
+func getGcm(key string) (cipher.AEAD, error) {
+	newCipher, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return nil, ErrVerifyKEy
+	}
+
+	gcm, err := cipher.NewGCM(newCipher)
+	if err != nil {
+		return nil, ErrBlockCipher
+	}
+
+	return gcm, nil
+}
+
+func Encrypt(plaintext string, key string) (string, error) {
+	gcm, err := getGcm(key)
+	if err != nil {
+		logger.Error(err)
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = rand.Read(nonce)
+	if err != nil {
+		logger.Error(err)
+		return "", ErrRandRead
+	}
+
+	encryptData := string(gcm.Seal(nonce, nonce, []byte(plaintext), nil))
+
+	return encryptData, nil
+}
+
+func Decrypt(ciphertext string, key string) (string, error) {
+	ciphertextToByte := []byte(ciphertext)
+
+	gcm, err := getGcm(key)
+	if err != nil {
+		logger.Error(err)
+		return "", err
+	}
+
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertextToByte := ciphertextToByte[:nonceSize], ciphertextToByte[nonceSize:]
+
+	plaintext, err := gcm.Open(nil, nonce, ciphertextToByte, nil)
+	if err != nil {
+		logger.Error(err)
+		return "", ErrAuthCiphertext
+	}
+
+	return string(plaintext), nil
+}
+
+func GenerateKey() (string, error) {
+	key := make([]byte, SizeKey)
+
+	_, err := rand.Read(key)
+	if err != nil {
+		logger.Error(err)
+		return "", ErrGenerateKey
+	}
+
+	return string(key), nil
 }
