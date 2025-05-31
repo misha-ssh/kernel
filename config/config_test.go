a45701b7d7a946ee38555b26ee6cc227341484b3
Denis <korbakovd@gmail.com>
2025-06-01 01:42:45 +0300

    #165 add test cases

--- config/config_test.go
+++ config/config_test.go
@@ -1,19 +1,38 @@
 package config
 
-import "testing"
+import (
+	"github.com/ssh-connection-manager/kernel/v2/internal/storage"
+	"os"
+	"testing"
+)
 
 func TestInit(t *testing.T) {
 	tests := []struct {
-		name    string
-		wantErr bool
+		name          string
+		wantErr       bool
+		deleteHomeDir bool
 	}{
 		{
-			name:    "success init",
-			wantErr: false,
+			name:          "create files with empty project dir",
+			wantErr:       false,
+			deleteHomeDir: true,
+		},
+		{
+			name:          "init at created needed files",
+			wantErr:       false,
+			deleteHomeDir: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if tt.deleteHomeDir {
+				err := os.RemoveAll(storage.GetHomeDir())
+
+				if err != nil {
+					t.Errorf("error at delete dir = %v, wantErr %v", err, tt.wantErr)
+				}
+			}
+
 			if err := Init(); (err != nil) != tt.wantErr {
 				t.Errorf("Init() error = %v, wantErr %v", err, tt.wantErr)
 			}
