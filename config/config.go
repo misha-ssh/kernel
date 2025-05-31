e1f770dddb0eeef38c68f9ef5c07b25b79e9eeb8
Denis <korbakovd@gmail.com>
2025-06-01 01:42:34 +0300

    #165 add logic create crypt key

--- config/config.go
+++ config/config.go
@@ -2,39 +2,36 @@ package config
 
 import (
 	"errors"
+	"os/user"
 
 	"github.com/ssh-connection-manager/kernel/v2/config/envconst"
 	"github.com/ssh-connection-manager/kernel/v2/config/envname"
 	"github.com/ssh-connection-manager/kernel/v2/internal/config"
+	"github.com/ssh-connection-manager/kernel/v2/internal/crypto"
 	"github.com/ssh-connection-manager/kernel/v2/internal/logger"
 	"github.com/ssh-connection-manager/kernel/v2/internal/storage"
+	"github.com/zalando/go-keyring"
 )
 
-var ErrGetConsoleInfo = errors.New("err set default value")
-
-func setDefaultValues(config config.Config) error {
-	if !config.Exists(envname.Theme) {
-		err := config.Set(envname.Theme, envconst.Theme)
-		if err != nil {
-			logger.Error(ErrGetConsoleInfo)
-			return ErrGetConsoleInfo
-		}
-	}
-
-	return nil
-}
+var (
+	ErrGetConsoleInfo       = errors.New("err set default value")
+	ErrCreateFileConnection = errors.New("err create file connection")
+	ErrGetCryptKey          = errors.New("err get crypt key")
+	ErrSetCryptKey          = errors.New("err set crypt key")
+)
 
 // todo добавить дефолтные значения при пустом файле и так же их шифрануть
 func initFileConnections() error {
-	fileName := envconst.FilenameConnection
+	filename := envconst.FilenameConnection
 
 	fileStorage := &storage.FileStorage{
 		Direction: storage.GetHomeDir(),
 	}
 
-	if !fileStorage.Exists(fileName) {
-		err := fileStorage.Create(envconst.FilenameConnection)
+	if !fileStorage.Exists(filename) {
+		err := fileStorage.Create(filename)
 		if err != nil {
+			logger.Error(ErrCreateFileConnection.Error())
 			return err
 		}
 	}
@@ -43,33 +40,87 @@ func initFileConnections() error {
 }
 
 func initFileConfig() error {
+	filename := envconst.FilenameConfig
+
 	fileStorage := &storage.FileStorage{
 		Direction: storage.GetHomeDir(),
+	}
+
+	if !fileStorage.Exists(filename) {
+		err := fileStorage.Create(filename)
+		if err != nil {
+			logger.Error(ErrCreateFileConnection.Error())
+			return err
+		}
 	}
 
 	fileConfig := &config.StorageConfig{
 		Storage: fileStorage,
 	}
 
-	err := setDefaultValues(fileConfig)
+	if !fileConfig.Exists(envname.Theme) {
+		err := fileConfig.Set(envname.Theme, envconst.Theme)
+		if err != nil {
+			logger.Error(ErrGetConsoleInfo.Error())
+			return ErrGetConsoleInfo
+		}
+	}
+
+	return nil
+}
+
+func initCryptKey() error {
+	currentUser, err := user.Current()
 	if err != nil {
 		logger.Error(err.Error())
 		return err
+	}
+
+	username := currentUser.Username
+
+	service := envconst.NameServiceCryptKey
+
+	cryptKey, err := keyring.Get(service, username)
+	if err != nil {
+		logger.Error(ErrGetCryptKey.Error())
+		return ErrGetCryptKey
+	}
+
+	if cryptKey == "" {
+		cryptKey, err = crypto.GenerateKey()
+		if err != nil {
+			logger.Error(err.Error())
+			return err
+		}
+
+		err = keyring.Set(service, username, cryptKey)
+		if err != nil {
+			logger.Error(ErrSetCryptKey.Error())
+			return ErrSetCryptKey
+		}
 	}
 
 	return nil
 }
 
 func Init() error {
-	err := initFileConfig()
+	var err error
+
+	err = initCryptKey()
 	if err != nil {
-		logger.Error(err)
+		logger.Error(err.Error())
+		return err
+	}
+
+	err = initFileConfig()
+	if err != nil {
+		logger.Error(err.Error())
 		return err
 	}
 
 	err = initFileConnections()
 	if err != nil {
-		logger.Error(err)
+		logger.Error(err.Error())
 		return err
 	}
 
