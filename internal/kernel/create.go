b3444ffdc8f8ae6cf937485a67a8e8d83377246e
Denis <korbakovd@gmail.com>
2025-06-03 00:18:55 +0300

    #165 create logic for create command

--- internal/kernel/create.go
+++ internal/kernel/create.go
@@ -1,4 +1,102 @@
 package kernel
 
-func Create() {
+import (
+	"encoding/json"
+	"errors"
+	"os/user"
+
+	"github.com/ssh-connection-manager/kernel/v2/config"
+	"github.com/ssh-connection-manager/kernel/v2/config/envconst"
+	"github.com/ssh-connection-manager/kernel/v2/internal/connect"
+	"github.com/ssh-connection-manager/kernel/v2/internal/crypto"
+	"github.com/ssh-connection-manager/kernel/v2/internal/logger"
+	"github.com/ssh-connection-manager/kernel/v2/internal/storage"
+	"github.com/zalando/go-keyring"
+)
+
+const (
+	FileConnections = envconst.FilenameConnection
+)
+
+var (
+	ErrMarshalJson             = errors.New("failed to marshal json")
+	ErrUnmarshalJson           = errors.New("failed to unmarshal json")
+	ErrWriteJson               = errors.New("failed to write json")
+	ErrGetCryptKey             = errors.New("err get crypt key")
+	ErrInitConfig              = errors.New("failed to init config")
+	ErrGetConnection           = errors.New("failed to get connection")
+	ErrDecryptData             = errors.New("failed to decrypt data")
+	ErrConnectionByAliasExists = errors.New("connection by alias exists")
+	ErrEncryptData             = errors.New("err encrypt data")
+)
+
+func Create(connection *connect.Connect, storage storage.Storage) error {
+	var connections connect.Connections
+
+	err := config.Init()
+	if err != nil {
+		logger.Error(ErrInitConfig.Error())
+		return ErrInitConfig
+	}
+
+	encryptedConnections, err := storage.Get(FileConnections)
+	if err != nil {
+		logger.Error(ErrGetConnection.Error())
+		return ErrGetConnection
+	}
+
+	currentUser, err := user.Current()
+	if err != nil {
+		logger.Error(err.Error())
+		return err
+	}
+
+	username := currentUser.Username
+
+	cryptKey, err := keyring.Get(envconst.NameServiceCryptKey, username)
+	if err != nil {
+		logger.Error(ErrGetCryptKey.Error())
+		return err
+	}
+
+	decryptedConnections, err := crypto.Decrypt(encryptedConnections, cryptKey)
+	if err != nil {
+		logger.Error(ErrDecryptData.Error())
+		return ErrDecryptData
+	}
+
+	err = json.Unmarshal([]byte(decryptedConnections), &connections)
+	if err != nil {
+		logger.Error(ErrUnmarshalJson.Error())
+		return ErrUnmarshalJson
+	}
+
+	for _, savedConnection := range connections.Connects {
+		if savedConnection.Alias == connection.Alias {
+			logger.Error(ErrConnectionByAliasExists.Error())
+			return ErrConnectionByAliasExists
+		}
+	}
+
+	connections.Connects = append(connections.Connects, *connection)
+
+	jsonConnections, err := json.Marshal(connections)
+	if err != nil {
+		logger.Error(ErrMarshalJson.Error())
+		return ErrMarshalJson
+	}
+
+	updatedEncryptedConnections, err := crypto.Encrypt(string(jsonConnections), cryptKey)
+	if err != nil {
+		logger.Error(ErrEncryptData.Error())
+		return ErrEncryptData
+	}
+
+	err = storage.Write(FileConnections, updatedEncryptedConnections)
+	if err != nil {
+		logger.Error(ErrWriteJson.Error())
+		return ErrWriteJson
+	}
+
+	return nil
 }
