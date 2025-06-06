1fb646ffdfb931e642b1cfac61db45362580463e
Denis <korbakovd@gmail.com>
2025-06-07 00:32:58 +0300

    #165 delete extra test cases

--- config/config_test.go
+++ config/config_test.go
@@ -1,42 +1,21 @@
 package config
 
 import (
-	"github.com/ssh-connection-manager/kernel/v2/config/envconst"
-	"github.com/ssh-connection-manager/kernel/v2/internal/crypto"
-	"github.com/ssh-connection-manager/kernel/v2/internal/storage"
-	"github.com/zalando/go-keyring"
-	"os"
-	"os/user"
 	"testing"
 )
 
 func Test_Init(t *testing.T) {
 	tests := []struct {
-		name          string
-		wantErr       bool
-		deleteHomeDir bool
+		name    string
+		wantErr bool
 	}{
 		{
-			name:          "create files with empty project dir",
-			wantErr:       false,
-			deleteHomeDir: true,
-		},
-		{
-			name:          "init at created needed files",
-			wantErr:       false,
-			deleteHomeDir: false,
+			name:    "success init",
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if tt.deleteHomeDir {
-				err := os.RemoveAll(storage.GetHomeDir())
-
-				if err != nil {
-					t.Errorf("error at delete dir = %v, wantErr %v", err, tt.wantErr)
-				}
-			}
-
 			if err := Init(); (err != nil) != tt.wantErr {
 				t.Errorf("Init() error = %v, wantErr %v", err, tt.wantErr)
 			}
@@ -46,53 +25,18 @@ func Test_Init(t *testing.T) {
 
 func Test_initCryptKey(t *testing.T) {
 	tests := []struct {
-		name        string
-		wantErr     bool
-		isDeleteKey bool
+		name    string
+		wantErr bool
 	}{
 		{
-			name:        "get save crypt key",
-			wantErr:     false,
-			isDeleteKey: false,
-		},
-		{
-			name:        "empty key - set crypt key",
-			wantErr:     false,
-			isDeleteKey: true,
-		},
-		{
-			name:        "get generated crypt key",
-			wantErr:     false,
-			isDeleteKey: false,
+			name:    "success",
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			currentUser, err := user.Current()
-			if err != nil {
-				t.Errorf("error get user = %v, wantErr %v", err, tt.wantErr)
-			}
-
-			username := currentUser.Username
-
-			if tt.isDeleteKey {
-				err = keyring.Delete(envconst.NameServiceCryptKey, username)
-				if err != nil {
-					t.Errorf("error at delete key = %v, wantErr %v", err, tt.wantErr)
-				}
-			}
-
-			if err = initCryptKey(); (err != nil) != tt.wantErr {
+			if err := initCryptKey(); (err != nil) != tt.wantErr {
 				t.Errorf("initCryptKey() error = %v, wantErr %v", err, tt.wantErr)
-			}
-
-			key, err := keyring.Get(envconst.NameServiceCryptKey, username)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("error get key = %v, wantErr %v", err, tt.wantErr)
-			}
-
-			if len(key) != crypto.SizeKey {
-				t.Errorf("invalid key = %v, want %v", key, crypto.SizeKey)
 			}
 		})
 	}
@@ -100,40 +44,18 @@ func Test_initCryptKey(t *testing.T) {
 
 func Test_initFileConfig(t *testing.T) {
 	tests := []struct {
-		name           string
-		wantErr        bool
-		isDeleteConfig bool
+		name    string
+		wantErr bool
 	}{
 		{
-			name:           "config is exist",
-			wantErr:        false,
-			isDeleteConfig: false,
-		},
-		{
-			name:           "config is empty - create config",
-			wantErr:        false,
-			isDeleteConfig: true,
+			name:    "success",
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			fileStorage := &storage.FileStorage{
-				Direction: storage.GetHomeDir(),
-			}
-
-			if tt.isDeleteConfig {
-				err := fileStorage.Delete(envconst.FilenameConfig)
-				if err != nil {
-					t.Errorf("error at delete config = %v, wantErr %v", err, tt.wantErr)
-				}
-			}
-
 			if err := initFileConfig(); (err != nil) != tt.wantErr {
 				t.Errorf("initFileConfig() error = %v, wantErr %v", err, tt.wantErr)
-			}
-
-			if !fileStorage.Exists(envconst.FilenameConfig) {
-				t.Errorf("config file not exist")
 			}
 		})
 	}
@@ -144,7 +66,10 @@ func Test_initFileConnections(t *testing.T) {
 		name    string
 		wantErr bool
 	}{
-		{},
+		{
+			name:    "success",
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
