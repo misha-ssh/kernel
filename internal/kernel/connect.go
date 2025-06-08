36a15444a831bd21cfc0a37ccff63118742d63d9
Denis <korbakovd@gmail.com>
2025-06-09 01:25:56 +0300

    #165 delete extra code

--- internal/kernel/connect.go
+++ internal/kernel/connect.go
@@ -1,8 +1,1 @@
 package kernel
-
-import (
-	"github.com/ssh-connection-manager/kernel/v2/internal/connect"
-	"github.com/ssh-connection-manager/kernel/v2/internal/storage"
-)
-
-func Connect(connection connect.Connect, storage storage.Storage){}
