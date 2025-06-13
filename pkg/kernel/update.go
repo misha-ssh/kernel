54af371f1294272839f253fa353b8bda90e61719
Denis <korbakovd@gmail.com>
2025-06-13 22:15:17 +0300

    #165 create update method

--- pkg/kernel/update.go
+++ pkg/kernel/update.go
@@ -1,3 +1,42 @@
 package kernel
 
-func Change(){}
+import (
+	"errors"
+
+	"github.com/ssh-connection-manager/kernel/v2/internal/connect"
+	"github.com/ssh-connection-manager/kernel/v2/internal/logger"
+	"github.com/ssh-connection-manager/kernel/v2/internal/setup"
+	"github.com/ssh-connection-manager/kernel/v2/internal/store"
+)
+
+var (
+	ErrNotFoundConnectionAtUpdate = errors.New("err not found connection")
+	ErrGetConnectionAtUpdate      = errors.New("err get connection")
+	ErrSetConnectionAtUpdate      = errors.New("err set connection")
+)
+
+func Update(connection *connect.Connect, oldAlias string) error {
+	setup.Init()
+
+	connections, err := store.GetConnections()
+	if err != nil {
+		logger.Error(ErrGetConnectionAtUpdate.Error())
+		return ErrGetConnectionAtUpdate
+	}
+
+	for _, savedConnection := range connections.Connects {
+		if savedConnection.Alias == oldAlias {
+			connections.Connects = append(connections.Connects, *connection)
+
+			err = store.SetConnections(connections)
+			if err != nil {
+				logger.Error(ErrSetConnectionAtUpdate.Error())
+				return ErrSetConnectionAtUpdate
+			}
+
+			return nil
+		}
+	}
+
+	return ErrNotFoundConnectionAtUpdate
+}
