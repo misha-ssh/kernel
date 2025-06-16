bab46ec67d2420ef497181cd87595863c5d5ab9b
Denis <korbakovd@gmail.com>
2025-06-16 21:30:27 +0300

    #165 create command list

--- pkg/kernel/list.go
+++ pkg/kernel/list.go
@@ -1,3 +1,24 @@
 package kernel
 
-func List() {}
+import (
+	"errors"
+
+	"github.com/ssh-connection-manager/kernel/v2/internal/connect"
+	"github.com/ssh-connection-manager/kernel/v2/internal/logger"
+	"github.com/ssh-connection-manager/kernel/v2/internal/setup"
+	"github.com/ssh-connection-manager/kernel/v2/internal/store"
+)
+
+var ErrGetConnectionAtList = errors.New("err get connections")
+
+func List() (*connect.Connections, error) {
+	setup.Init()
+
+	connections, err := store.GetConnections()
+	if err != nil {
+		logger.Error(ErrGetConnectionAtList.Error())
+		return nil, ErrGetConnectionAtList
+	}
+
+	return connections, nil
+}
