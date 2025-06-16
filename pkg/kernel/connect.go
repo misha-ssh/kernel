ce0f9a971a9a6ef12c3d37fd19ca3f6503e9d0c2
Denis <korbakovd@gmail.com>
2025-06-16 22:02:29 +0300

    #165 create example logic command create

--- pkg/kernel/connect.go
+++ pkg/kernel/connect.go
@@ -1,3 +1,30 @@
 package kernel
 
-func Connect() {}
+import (
+	"errors"
+	"github.com/ssh-connection-manager/kernel/v2/internal/connect"
+	"github.com/ssh-connection-manager/kernel/v2/internal/logger"
+	"github.com/ssh-connection-manager/kernel/v2/internal/setup"
+)
+
+var (
+	ErrSshConnect  = errors.New("err ssh connect")
+	ErrTypeConnect = errors.New("err type connect")
+)
+
+func Connect(connection *connect.Connect) error {
+	setup.Init()
+
+	switch connection.Type {
+	case connect.TypeSSH:
+		sshConnect := connect.NewSshConnect()
+		err := sshConnect.Connect(connection)
+		if err != nil {
+			logger.Error(ErrSshConnect.Error())
+			return ErrSshConnect
+		}
+		return nil
+	default:
+		return ErrTypeConnect
+	}
+}
