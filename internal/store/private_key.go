9c5477c90e9da2e224133ee015cd4ad628fb4eb3
Denis <korbakovd@gmail.com>
2025-06-22 17:09:58 +0300

    #165 delete logic for crypt keys

--- internal/store/private_key.go
+++ internal/store/private_key.go
@@ -5,10 +5,8 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
-	"path/filepath"
 
 	"github.com/ssh-connection-manager/kernel/v2/internal/connect"
-	"github.com/ssh-connection-manager/kernel/v2/internal/crypto"
 	"github.com/ssh-connection-manager/kernel/v2/internal/logger"
 	"github.com/ssh-connection-manager/kernel/v2/internal/storage"
 )
@@ -16,12 +14,10 @@ import (
 var (
 	DirectionKeys = storage.GetPrivateKeysDir()
 
-	ErrGetCryptKeyAtPrivateKey = errors.New("err get crypt key")
-	ErrWriteToFilePrivateKey   = errors.New("err write to file private key")
-	ErrCreateFilePrivateKey    = errors.New("err create file private key")
-	ErrNotValidPrivateKey      = errors.New("private key is not valid")
-	ErrGetDataPrivateKey       = errors.New("private key get data error")
-	ErrEncryptPrivateKey       = errors.New("err encrypt private key")
+	ErrWriteToFilePrivateKey = errors.New("err write to file private key")
+	ErrCreateFilePrivateKey  = errors.New("err create file private key")
+	ErrNotValidPrivateKey    = errors.New("private key is not valid")
+	ErrGetDataPrivateKey     = errors.New("private key get data error")
 )
 
 func validatePrivateKey(privateKey string) error {
@@ -49,9 +45,7 @@ func validatePrivateKey(privateKey string) error {
 }
 
 func SavePrivateKey(connection *connect.Connect) (string, error) {
-	direction := filepath.Dir(connection.SshOptions.PrivateKey)
-	filename := filepath.Base(connection.SshOptions.PrivateKey)
-
+	direction, filename := storage.GetDirectionAndFilename(connection.SshOptions.PrivateKey)
 	dataPrivateKey, err := storage.Get(direction, filename)
 	if err != nil {
 		logger.Error(ErrGetDataPrivateKey.Error())
@@ -64,18 +58,6 @@ func SavePrivateKey(connection *connect.Connect) (string, error) {
 		return "", ErrNotValidPrivateKey
 	}
 
-	cryptKey, err := GetCryptKey()
-	if err != nil {
-		logger.Error(ErrGetCryptKeyAtPrivateKey.Error())
-		return "", ErrGetCryptKeyAtPrivateKey
-	}
-
-	encryptDataPrivateKey, err := crypto.Encrypt(dataPrivateKey, cryptKey)
-	if err != nil {
-		logger.Error(ErrEncryptPrivateKey.Error())
-		return "", ErrEncryptPrivateKey
-	}
-
 	filenamePrivateKey := connection.Alias
 
 	err = storage.Create(DirectionKeys, filenamePrivateKey)
@@ -84,34 +66,13 @@ func SavePrivateKey(connection *connect.Connect) (string, error) {
 		return "", ErrCreateFilePrivateKey
 	}
 
-	err = storage.Write(DirectionKeys, filenamePrivateKey, encryptDataPrivateKey)
+	err = storage.Write(DirectionKeys, filenamePrivateKey, dataPrivateKey)
 	if err != nil {
 		logger.Error(ErrWriteToFilePrivateKey.Error())
 		return "", ErrWriteToFilePrivateKey
 	}
 
-	savedPrivateKey := filepath.Join(DirectionKeys, filenamePrivateKey)
-
-	return savedPrivateKey, nil
-}
-
-func GetPrivateKey(connection *connect.Connect) (string, error) {
-	directionPrivateKey := storage.GetPrivateKeysDir()
-	filenamePrivateKey := connection.Alias
-
-	encryptDataPrivateKey, err := storage.Get(directionPrivateKey, filenamePrivateKey)
-	if err != nil {
-		logger.Error(ErrGetDataPrivateKey.Error())
-		return "", ErrGetDataPrivateKey
-	}
-
-	cryptKey, err := GetCryptKey()
-	if err != nil {
-		logger.Error(ErrGetCryptKeyAtPrivateKey.Error())
-		return "", ErrGetCryptKeyAtPrivateKey
-	}
-
-	return crypto.Decrypt(encryptDataPrivateKey, cryptKey)
+	return storage.GetFullPath(DirectionKeys, filenamePrivateKey), nil
 }
 
 func DeletePrivateKey(connection *connect.Connect) error {
