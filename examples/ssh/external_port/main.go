1d3f96cafbabc81e90f3a340dfd8f0f61aae3735
Denis <korbakovd@gmail.com>
2025-06-27 21:37:42 +0300

    #165 update examples for connect

--- examples/ssh/external_port/main.go
+++ examples/ssh/external_port/main.go
@@ -2,6 +2,7 @@ package main
 
 import "github.com/ssh-connection-manager/kernel/v2/internal/connect"
 
+// main for success connect start make command: up-ssh-port
 func main() {
 	connection := &connect.Connect{
 		Alias:     "test",
@@ -12,14 +13,26 @@ func main() {
 		CreatedAt: "",
 		UpdatedAt: "",
 		SshOptions: &connect.SshOptions{
-			Port:       22,
-			PrivateKey: false,
+			Port:       2222,
+			PrivateKey: "",
 		},
 	}
-	conn := connect.SshConnect{}
 
-	err := conn.Connect(connection)
+	sshConnect := connect.NewSshConnect()
+	session, err := sshConnect.Connect(connection)
 	if err != nil {
 		panic(err)
 	}
+
+	err = session.Shell()
+	if err != nil {
+		panic(err)
+	}
+
+	err = session.Wait()
+	if err != nil {
+		panic(err)
+	}
+
+	defer session.Close()
 }
---
+++ examples/ssh/private_key/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "github.com/ssh-connection-manager/kernel/v2/internal/connect"
+
+// main for success connect start make command: up-ssh-key
+func main() {
+	connection := &connect.Connect{
+		Alias:     "test",
+		Login:     "root",
+		Password:  "password",
+		Address:   "localhost",
+		Type:      connect.TypeSSH,
+		CreatedAt: "",
+		UpdatedAt: "",
+		SshOptions: &connect.SshOptions{
+			Port:       22,
+			PrivateKey: "./dockerkey",
+		},
+	}
+
+	sshConnect := connect.NewSshConnect()
+	session, err := sshConnect.Connect(connection)
+	if err != nil {
+		panic(err)
+	}
+
+	err = session.Shell()
+	if err != nil {
+		panic(err)
+	}
+
+	err = session.Wait()
+	if err != nil {
+		panic(err)
+	}
+
+	defer session.Close()
+}
