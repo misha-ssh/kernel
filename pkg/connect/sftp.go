3fabedacfdf3699555cd20c5e0087829e1a2cd11
Denis <korbakovd@gmail.com>
2025-09-25 12:02:13 +0300

    fix: update arg in new sftp method

--- pkg/connect/sftp.go
+++ pkg/connect/sftp.go
@@ -4,7 +4,7 @@ import "github.com/pkg/sftp"
 
 type Sftp struct{}
 
-func NewSftp(connection *Connect) (*sftp.Client, error) {
+func NewSftp(connection *Connect, opts ...sftp.ClientOption) (*sftp.Client, error) {
 	ssh := Ssh{}
 
 	client, err := ssh.Client(connection)
@@ -12,5 +12,5 @@ func NewSftp(connection *Connect) (*sftp.Client, error) {
 		return nil, err
 	}
 
-	return sftp.NewClient(client, nil)
+	return sftp.NewClient(client, opts...)
 }
