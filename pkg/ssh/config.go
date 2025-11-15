8f34c484dbd59e91b4a2d2501490ed61ef38b111
Denis <korbakovd@gmail.com>
2025-11-15 17:05:12 +0300

    feat: add base method

--- pkg/ssh/config.go
+++ pkg/ssh/config.go
@@ -2,6 +2,7 @@ package ssh
 
 import (
 	"github.com/misha-ssh/kernel/internal/storage"
+	"github.com/misha-ssh/kernel/pkg/connect"
 )
 
 type Config struct {
@@ -12,4 +13,20 @@ func NewConfig() *Config {
 	return &Config{
 		Path: storage.GetDirSSH(),
 	}
+}
+
+func (c *Config) GetConnections() (*connect.Connections, error) {
+	return nil, nil
+}
+
+func (c *Config) SaveConnection(connection *connect.Connect) error {
+	return nil
+}
+
+func (c *Config) UpdateConnection(connection *connect.Connect) (*connect.Connect, error) {
+	return nil, nil
+}
+
+func (c *Config) DeleteConnection(connection *connect.Connect) error {
+	return nil
 }
