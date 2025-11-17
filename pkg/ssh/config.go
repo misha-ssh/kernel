68a534f579bd976a79bc961627d927e466a7e301
Denis <korbakovd@gmail.com>
2025-11-17 21:24:02 +0300

    feat: add bse logic

--- pkg/ssh/config.go
+++ pkg/ssh/config.go
@@ -1,21 +1,32 @@
 package ssh
 
 import (
+	"fmt"
+	"github.com/misha-ssh/kernel/configs/envconst"
 	"github.com/misha-ssh/kernel/internal/storage"
 	"github.com/misha-ssh/kernel/pkg/connect"
 )
 
 type Config struct {
-	Path string
+	LocalStorage *storage.Local
 }
 
 func NewConfig() *Config {
 	return &Config{
-		Path: storage.GetDirSSH(),
+		LocalStorage: &storage.Local{
+			Path: storage.GetDirSSH(),
+		},
 	}
 }
 
 func (c *Config) GetConnections() (*connect.Connections, error) {
+	data, err := c.LocalStorage.Get(envconst.FilenameConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(data)
+
 	return nil, nil
 }
 
