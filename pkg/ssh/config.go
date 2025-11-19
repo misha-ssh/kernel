d49a5302c5c2a1b71ffc985b7dc570017082c7d0
Denis <korbakovd@gmail.com>
2025-11-20 01:58:04 +0300

    feat: add base logic for set alias

--- pkg/ssh/config.go
+++ pkg/ssh/config.go
@@ -1,10 +1,13 @@
 package ssh
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/misha-ssh/kernel/configs/envconst"
 	"github.com/misha-ssh/kernel/internal/storage"
 	"github.com/misha-ssh/kernel/pkg/connect"
+	"os"
+	"strings"
 )
 
 type Config struct {
@@ -20,12 +23,41 @@ func NewConfig() *Config {
 }
 
 func (c *Config) GetConnections() (*connect.Connections, error) {
-	data, err := c.LocalStorage.Get(envconst.FilenameConfig)
+	var connections connect.Connections
+
+	file, err := c.LocalStorage.GetOpenFile(envconst.FilenameConfigSSH, os.O_RDWR)
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Println(data)
+	s := bufio.NewScanner(file)
+
+	for s.Scan() {
+		line := strings.TrimSpace(s.Text())
+
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		if strings.Contains(line, "Host") {
+			value := strings.Split(line, " ")
+
+			if value[0] == "Host" {
+				connection := new(connect.Connect)
+				connection.Alias = line[5:]
+
+				connections.Connects = append(connections.Connects, *connection)
+			}
+		}
+
+		fmt.Println(line)
+	}
+
+	fmt.Println(connections)
+
+	if err = s.Err(); err != nil {
+		fmt.Println("err: ", err)
+	}
 
 	return nil, nil
 }
