7c6a8a91d642ac5ae4abcf77ef7de4dcc5775f11
Denis <korbakovd@gmail.com>
2025-11-21 00:31:13 +0300

    feat: add base logic add data from config

--- pkg/ssh/config.go
+++ pkg/ssh/config.go
@@ -3,11 +3,14 @@ package ssh
 import (
 	"bufio"
 	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+
 	"github.com/misha-ssh/kernel/configs/envconst"
 	"github.com/misha-ssh/kernel/internal/storage"
 	"github.com/misha-ssh/kernel/pkg/connect"
-	"os"
-	"strings"
 )
 
 type Config struct {
@@ -32,32 +35,63 @@ func (c *Config) GetConnections() (*connect.Connections, error) {
 
 	s := bufio.NewScanner(file)
 
+	connection := new(connect.Connect)
+
 	for s.Scan() {
 		line := strings.TrimSpace(s.Text())
+		fmt.Println(line)
 
 		if strings.HasPrefix(line, "#") {
 			continue
 		}
 
-		//todo it is necessary to make a strict comparison to distinguish between Host and Hostname
-		if strings.Contains(line, "Host") {
-			value := strings.Split(line, " ")
+		value := strings.Split(line, " ")
 
-			if value[0] == "Host" {
-				connection := new(connect.Connect)
-				connection.Alias = line[5:]
-
+		if line == "" {
+			if !reflect.DeepEqual(connection, &connect.Connect{}) {
 				connections.Connects = append(connections.Connects, *connection)
 			}
+
+			connection = new(connect.Connect)
 		}
 
-		fmt.Println(line)
+		if value[0] == "Host" {
+			if len(value) < 1 {
+				continue
+			}
+
+			if strings.Contains(value[1], "*") || strings.Contains(value[1], "!") {
+				continue
+			}
+
+			connection.Alias = value[1]
+		}
+
+		if value[0] == "HostName" {
+			connection.Address = value[1]
+		}
+
+		if value[0] == "User" {
+			connection.Login = value[1]
+		}
+
+		if value[0] == "Port" {
+			port, err := strconv.Atoi(value[1])
+			if err != nil {
+				return nil, err
+			}
+			connection.Port = port
+		}
+
+		if value[0] == "IdentityFile" {
+			connection.PrivateKey = value[1]
+		}
 	}
 
 	fmt.Println(connections)
 
 	if err = s.Err(); err != nil {
-		fmt.Println("err: ", err)
+		return nil, err
 	}
 
 	return nil, nil
