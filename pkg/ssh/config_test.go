63bd17b9645bee8552589a6817d20824cc47d4f6
Denis <korbakovd@gmail.com>
2025-11-29 15:16:45 +0300

    feat: add example logic for create config from testdata

--- pkg/ssh/config_test.go
+++ pkg/ssh/config_test.go
@@ -1,34 +1,50 @@
 package ssh
 
 import (
+	"github.com/misha-ssh/kernel/internal/storage"
+	"github.com/misha-ssh/kernel/testutil"
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/misha-ssh/kernel/pkg/connect"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestConfig_GetConnections(t *testing.T) {
 	tests := []struct {
-		name    string
-		want    *connect.Connections
-		wantErr bool
+		name     string
+		want     *connect.Connections
+		filename string
+		wantErr  bool
 	}{
 		{
-			name:    "success - get connections",
-			want:    &connect.Connections{},
-			wantErr: false,
+			name:     "success - get connections",
+			want:     &connect.Connections{},
+			filename: "testdata/config",
+			wantErr:  false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			config := NewConfig()
+			tmpDir := t.TempDir()
+
+			err := testutil.CreateSSHConfig(tmpDir, tt.filename)
+			require.NoError(t, err)
+
+			_, err = testutil.CreatePrivateKey(tmpDir)
+			require.NoError(t, err)
+
+			config := &Config{
+				LocalStorage: &storage.Local{
+					Path: tmpDir,
+				},
+			}
 
 			got, err := config.GetConnections()
 			if !tt.wantErr {
-				assert.NoError(t, err)
+				require.NoError(t, err)
 			}
 
-			assert.Equal(t, tt.want, got)
+			require.Equal(t, tt.want, got)
 		})
 	}
 }
