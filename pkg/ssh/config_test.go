c21647391e915a9455fa006b3c69659724d31962
Denis <korbakovd@gmail.com>
2025-11-30 20:53:41 +0300

    feat: add new test

--- pkg/ssh/config_test.go
+++ pkg/ssh/config_test.go
@@ -1,9 +1,13 @@
 package ssh
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/misha-ssh/kernel/internal/storage"
 	"github.com/misha-ssh/kernel/testutil"
 	"github.com/stretchr/testify/require"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/misha-ssh/kernel/pkg/connect"
@@ -68,6 +72,91 @@ func TestConfig_GetConnections(t *testing.T) {
 			}
 
 			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestConfig_SaveConnection(t *testing.T) {
+	tests := []struct {
+		name       string
+		connection *connect.Connect
+		filename   string
+		wantErr    bool
+	}{
+		{
+			name: "success - add connections",
+			connection: &connect.Connect{
+				Alias:      "new",
+				Address:    "localhost",
+				Login:      "user",
+				Port:       3333,
+				PrivateKey: "testdata/private_key",
+			},
+			filename: "testdata/config",
+			wantErr:  false,
+		},
+		{
+			name: "success - empty file",
+			connection: &connect.Connect{
+				Alias:      "test",
+				Address:    "localhost",
+				Login:      "user",
+				Port:       3333,
+				PrivateKey: "testdata/private_key",
+			},
+			filename: "testdata/empty_config",
+			wantErr:  false,
+		},
+		{
+			name: "fail - exists alias",
+			connection: &connect.Connect{
+				Alias:      "test",
+				Address:    "localhost",
+				Login:      "user",
+				Port:       3333,
+				PrivateKey: "testdata/private_key",
+			},
+			filename: "testdata/config",
+			wantErr:  true,
+		},
+		{
+			name: "fail - invalid connection",
+			connection: &connect.Connect{
+				Alias: "test",
+			},
+			filename: "",
+			wantErr:  true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tmpDir := t.TempDir()
+
+			if tt.filename != "" {
+				err := testutil.CreateSSHConfig(tmpDir, tt.filename)
+				require.NoError(t, err)
+			}
+
+			config := &Config{
+				LocalStorage: &storage.Local{
+					Path: tmpDir,
+				},
+			}
+
+			err := config.SaveConnection(tt.connection)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+
+			// todo delete before finished method
+			file, err := os.OpenFile(filepath.Join(tmpDir, "config"), os.O_RDONLY, 0)
+			s := bufio.NewScanner(file)
+
+			for s.Scan() {
+				fmt.Println(s.Text())
+			}
 		})
 	}
 }
